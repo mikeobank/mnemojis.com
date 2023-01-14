b5b916306a46d80b853618d668716c3b43ab1edb
Mike O'Bank <mike.obank@pm.me>
2023-01-14 13:18:17 +0100

    Lib: filter

--- js/lib.js
+++ js/lib.js
@@ -1,10 +1,28 @@
 // dependent on mnemojis.json.js
+// dependent on lodash.toArray
+
+const isNotUndefined = v => v !== undefined
+
+const findMnemojiIndex = mnemoji => mnemojis.findIndex(({ chars }) => chars.includes(mnemoji))
+const isMnemoji = mnemoji => findMnemojiIndex(mnemoji) > -1
 
 const mapToMnemoji = i => mnemojis[i].chars[0]
 
-const mapFromMnemoji = mnemoji => mnemojis.findIndex(({ chars }) => chars[0] === mnemoji)
+const mapFromMnemoji = mnemoji => {
+  const index = findMnemojiIndex(mnemoji)
+  return index > -1 ? index : undefined
+}
 
-const mnemojisToBytes = s => Uint8Array.from(toArray(s).map(mnemoji => mapFromMnemoji(mnemoji))).filter(b => b > -1)
+const mapToPrimaryMnemoji = mnemoji => {
+  const index = findMnemojiIndex(mnemoji)
+  return index > -1 ? mapToMnemoji(index) : undefined
+}
+
+const mnemojisToBytes = s => Uint8Array.from(toArray(s).map(mapFromMnemoji).filter(isNotUndefined))
+
+const filterMnemojis = s => {
+  return toArray(s).map(mapToPrimaryMnemoji).filter(isNotUndefined).join("")
+}
 
 const randomMnemoji = () => {
   const values = window.crypto.getRandomValues(new Uint8Array(1))
